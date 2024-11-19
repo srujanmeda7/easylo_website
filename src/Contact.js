bab5db14351da612ca2beed8154130baf0a47984
srujanmeda7 <msrujan2002@gamil.in>
2024-11-19 15:37:01 +0530

    updated header

--- src/Contact.js
+++ src/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
     </div>
     <div className='container1'>
       <div className='map_section'>
-
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4325.919165810755!2d78.39767108368311!3d17.493623140480842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91b5f7d76543%3A0x97fd443d27eecbc6!2s7iTECH%20SOLUTIONS%20BEST%20SOFTWARE%20TRAINING%20INSTITUTE%20IN%20HYDERABAD%20%7C%20FULL%20STACK%20JAVA%7CPYTHON%7CPHP%7C.NET%7CDIGITALMARKETING%20%7CPLACEMENTS!5e0!3m2!1sen!2sin!4v1732010046386!5m2!1sen!2sin" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
     <div className='container1'>
