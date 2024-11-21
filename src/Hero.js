965393f4e847895f695ee70abe5d2638116243d8
srujanmeda7 <msrujan2002@gamil.in>
2024-11-21 14:53:44 +0530

    hero form updated

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Hero() {
+  const[dropdown_icon,setdropdown_icon]=useState(false);
+  const[dropdown_icon2,setdropdown_icon2]=useState(false);
+  const[select1,set_select1]=useState("Select Amount");
+
+  
+  console.log(dropdown_icon)  
+ 
   return (
     <>
      <div className='hero_content'>
@@ -24,22 +31,47 @@ function Hero() {
                       <div className='form_body'>
                       <label>AMOUNT YOU WANT</label>
                         <br></br>
-                        <select required="">
-                          <option>Select Amount</option>
-                          <option value="category1">category1</option>
-                          <option value="category2">category2</option>
-                          <option value="category3">category3</option>
-                        </select>
+                       <div className='select-menu' onClick={()=>{setdropdown_icon(!dropdown_icon)}}>
+                        <div className='select-btn' >
+                          
+                          <span className='btn-text'>{select1}</span>
+                          <span><i class="bi bi-chevron-down" style={{ transform: dropdown_icon ? 'rotate(180deg)' : 'none' }}></i></span>
+                        </div>
+                        <ul className={`options ${dropdown_icon?"open":""}`}>
+                          <li className='option' onClick={()=>{set_select1("category 1")}} >
+                            category 1
+                          </li>
+                          <li className='option' onClick={()=>{set_select1("category 2")}}>
+                          category 2
+                          </li>
+                          <li className='option' onClick={(e)=>{set_select1("category 3")}}>
+                          category 3
+                          </li>
+
+                        </ul>
+                       </div>
                         <br></br>
-                        <label>MONTH</label>
+                        <label>AMOUNT YOU WANT</label>
                         <br></br>
-                        <select>
-                          <option>Select Amount</option>
-                          <option value="category1">category1</option>
-                          <option value="category2">category2</option>
-                          <option value="category3">category3</option>
-                        </select>
-                        <p>YOU HAVE TO PAY:<span>$0</span></p>
+                       <div className='select-menu' onClick={()=>{setdropdown_icon2(!dropdown_icon2)}}>
+                        <div className='select-btn' >
+                          <span className='btn-text'>Select Month</span>
+                          <span><i class="bi bi-chevron-down" style={{ transform: dropdown_icon2? 'rotate(180deg)' : 'none' }}></i></span>
+                        </div>
+                        <ul className={`options ${dropdown_icon2?"open":""}`}>
+                          <li className='option'  onClick={()=>{set_select1("category 1")}}>
+                          category 1
+                          </li>
+                          <li className='option'  onClick={()=>{set_select1("category 2")}}>
+                          category 2
+                          </li>
+                          <li className='option'  onClick={()=>{set_select1("category 3")}}>
+                          category 3
+                          </li>
+
+                        </ul>
+                       </div>
+                        <p>YOU HAVE TO PAY:<span className='dollar'>$0</span></p>
                         <a href='#'>APPLY NOW</a>
                         <h5>We provide online instant cash loan</h5>
                       </div>
